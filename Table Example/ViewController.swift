019d8e829a6ba400ea423f435c93447c07d836e7
lopezzj1 <lopezzj1@uw.edu>
2015-11-03 16:32:48 -0800

    code for iquiz with a table view, title, subtitle, and an image

--- Table Example/ViewController.swift
+++ Table Example/ViewController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var quizTitles : [String] = ["Mathematics", "Marvel Superheros", "Science"]
+    var quizSubtitle : [String] = ["A quiz on simple math in addition, subtraction, multiplication, and division", "A quiz on Marvel's superheroes", "A quiz on scienc-y stuff"]
+
+    
+    var quizImage : [UIImage] = [
+        (UIImage(named: "calc.png"))!,
+        (UIImage(named: "superhero.png"))!,
+        (UIImage(named: "science.png"))!
+    
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.estimatedRowHeight = 44.0
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +36,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizTitles.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        cell.textLabel!.text = quizTitles[indexPath.row]
+        cell.detailTextLabel?.text = quizSubtitle[indexPath.row]
+        cell.imageView?.image = quizImage[indexPath.row]
+        
+        return cell
+    }
+    
+    @IBAction func settingsAlert(sender: AnyObject) {
+        
+        let alertController = UIAlertController(title: "Settings", message:
+            "Settings Go Here!", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alertController.addAction(UIAlertAction(title: "CANCEL", style: UIAlertActionStyle.Cancel,handler: nil))
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,handler: nil))
 
-
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+ 
 }
 
