4b7d0d9500d09dcf9f0acb85282fa06eb6b16282
lopezzj1 <lopezzj1@uw.edu>
2015-11-10 19:23:59 -0800

    added a game view controller with quiz question and answer

---
+++ Table Example/GameController.swift
@@ -0,0 +1,196 @@
+//
+//  GameController.swift
+//  iQuiz
+//
+//  Created by Jill Lopez on 11/10/15.
+//  Copyright © 2015 Jill Lopez. All rights reserved.
+//
+
+import UIKit
+
+class GameView: UIViewController {
+    
+    var selectedQuiz = ""
+    
+    var mathQuiz = ["What is 4 * 5?", "18", "20", "24", "0", "2"]
+    
+    var heroesQuiz = ["Who is Iron Man?", "Tony Stark", "Albert Einstein", "A normal person", "Iron Man is secretly Loki", "1"]
+    
+    var scienceQuiz = ["Which waves are used to make and receive cellphone calls?", "Radio waves", "Sound Waves", "Tidal Waves", "Electrowaves", "1"]
+    
+    var currentQuiz : [String] = []
+    
+    var buttonSelected = 0 //int to determine which button was pressed
+    
+    var isAnswerCorrect = false //boolean to determine if the correct answer was selected
+    
+    var count = 0;
+
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    
+    //answer one
+    @IBOutlet weak var ansOneButton: UIButton!
+    @IBAction func ansOneSelect(sender: AnyObject) {
+        if currentQuiz[5] == "1" {
+            isAnswerCorrect = true
+            count++
+            print("Correct!")
+            
+        }
+        
+        ansOneButton.enabled = false
+        ansTwoButton.enabled = false
+        ansThreeButton.enabled = false
+        ansFourButton.enabled = false
+        
+        
+        
+        //button border
+        ansOneButton.layer.borderWidth = 3;
+        ansOneButton.layer.borderColor = UIColor(red: 71/255, green: 204/242, blue: 242/255, alpha: 1.0).CGColor
+        
+        buttonSelected = 1
+        
+    }
+    
+    //answer two
+    @IBOutlet weak var ansTwoButton: UIButton!
+    @IBAction func ansTwoSelect(sender: AnyObject) {
+        if currentQuiz[5] == "2" {
+            isAnswerCorrect = true
+            count++
+            print("Correct!")
+        }
+        
+        ansOneButton.enabled = false
+        ansTwoButton.enabled = false
+        ansThreeButton.enabled = false
+        ansFourButton.enabled = false
+        
+        //button border
+        ansTwoButton.layer.borderWidth = 3;
+        ansTwoButton.layer.borderColor = UIColor(red: 71/255, green: 204/242, blue: 242/255, alpha: 1.0).CGColor
+    
+        buttonSelected = 2
+
+    }
+    
+    //answer three
+    @IBOutlet weak var ansThreeButton: UIButton!
+    @IBAction func ansThreeSelected(sender: AnyObject) {
+        if currentQuiz[5] == "3" {
+            isAnswerCorrect = true
+            count++
+            print("Correct!")
+        }
+        
+        ansOneButton.enabled = false
+        ansTwoButton.enabled = false
+        ansThreeButton.enabled = false
+        ansFourButton.enabled = false
+        
+        
+        //button border
+        ansThreeButton.layer.borderWidth = 3;
+        ansThreeButton.layer.borderColor = UIColor(red: 71/255, green: 204/242, blue: 242/255, alpha: 1.0).CGColor
+        
+        buttonSelected = 3
+
+    }
+    
+    
+    //answer four
+    @IBOutlet weak var ansFourButton: UIButton!
+    @IBAction func ansFourSelected(sender: AnyObject) {
+        if currentQuiz[5] == "4" {
+            isAnswerCorrect = true
+            count++
+            print("Correct!")
+
+        }
+        
+        ansOneButton.enabled = false
+        ansTwoButton.enabled = false
+        ansThreeButton.enabled = false
+        ansFourButton.enabled = false
+        
+        
+        //button border
+        ansFourButton.layer.borderWidth = 3;
+        ansFourButton.layer.borderColor = UIColor(red: 71/255, green: 204/242, blue: 242/255, alpha: 1.0).CGColor
+        
+        buttonSelected = 4
+        
+
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //load view
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        if selectedQuiz == "Mathematics" {
+            self.title = selectedQuiz
+            currentQuiz = mathQuiz
+            
+            questionLabel!.text? = mathQuiz[0]
+            
+            ansOneButton.setTitle(mathQuiz[1], forState: .Normal)
+            ansTwoButton.setTitle(mathQuiz[2], forState: .Normal)
+            ansThreeButton.setTitle(mathQuiz[3], forState: .Normal)
+            ansFourButton.setTitle(mathQuiz[4], forState: .Normal)
+            
+            
+        } else if selectedQuiz == "Marvel Superheros" {
+            self.title = selectedQuiz
+            currentQuiz = heroesQuiz
+            
+            questionLabel!.text? = heroesQuiz[0]
+            
+            ansOneButton.setTitle(heroesQuiz[1], forState: .Normal)
+            ansTwoButton.setTitle(heroesQuiz[2], forState: .Normal)
+            ansThreeButton.setTitle(heroesQuiz[3], forState: .Normal)
+            ansFourButton.setTitle(heroesQuiz[4], forState: .Normal)
+            
+            
+        } else if selectedQuiz == "Science" {
+            self.title = selectedQuiz
+            currentQuiz = scienceQuiz
+            
+            questionLabel!.text? = scienceQuiz[0]
+            
+            ansOneButton.setTitle(scienceQuiz[1], forState: .Normal)
+            ansTwoButton.setTitle(scienceQuiz[2], forState: .Normal)
+            ansThreeButton.setTitle(scienceQuiz[3], forState: .Normal)
+            ansFourButton.setTitle(scienceQuiz[4], forState: .Normal)
+            
+            
+        }
+        
+    }
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBAction func submitSelected(sender: AnyObject) {
+        
+        
+        
+        
+        
+        print("submit")
+    }
+
+    
+    
+    
+    
+}
--- Table Example/ViewController.swift
+++ Table Example/ViewController.swift
@@ -6,42 +6,68 @@
 //  Copyright © 2015 Jill Lopez. All rights reserved.
 //
 
+//
+//  ViewController.swift
+//  Table Example
+//
+//  Created by Jill Lopez on 11/1/15.
+//  Copyright © 2015 Jill Lopez. All rights reserved.
+//
+
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     @IBOutlet var tableView: UITableView!
+    
     
     var quizTitles : [String] = ["Mathematics", "Marvel Superheros", "Science"]
     var quizSubtitle : [String] = ["A quiz on simple math in addition, subtraction, multiplication, and division", "A quiz on Marvel's superheroes", "A quiz on scienc-y stuff"]
-
+    
     
     var quizImage : [UIImage] = [
         (UIImage(named: "calc.png"))!,
         (UIImage(named: "superhero.png"))!,
         (UIImage(named: "science.png"))!
-    
+        
     ]
+    
+    
+    var selectedTitle : String = ""
+    
+    /* Quizes */
+    var mathQuiz = ["Mathematics", "What is 2+2", "3", "4", "5", "6", "4"]
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "iQuiz"
         
-        self.tableView.estimatedRowHeight = 50
-        self.tableView.rowHeight = UITableViewAutomaticDimension
+        //self.tableView.estimatedRowHeight = 50
+        //self.tableView.rowHeight = UITableViewAutomaticDimension
         
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // return how many sections are in table
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //creates the number of rows/cells needed
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return quizTitles.count
     }
     
-    
+    //creates the title, subtitle, and image for each cell
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
         
@@ -52,21 +78,57 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
+    //alert for settings
     @IBAction func settingsAlert(sender: AnyObject) {
         
         let alertController = UIAlertController(title: "Settings", message: "Settings Go Here!", preferredStyle: UIAlertControllerStyle.Alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,handler: nil))
-
+        
         self.presentViewController(alertController, animated: true, completion: nil)
     }
     
+    //row Height
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath)
         -> CGFloat {
             //return indexPath.row == 0 ? 120 : 70
             return 70
     }
     
- 
+    //gets the cell that was selected
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        self.selectedTitle = (cell?.textLabel?.text)!
+        self.performSegueWithIdentifier("GameSegue", sender: nil)
+    }
+    
+    
+    //prepares for segue (Main)
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+       /* if selectedTitle == "Mathematics" {
+            if let gameController = segue.destinationViewController as? GameView {
+                gameController.selectedQuiz = "Mathematics"
+            }
+        }
+        
+        if selectedTitle == "Marvel Superheroes" {
+            if let gameController = segue.destinationViewController as? GameView {
+                gameController.selectedQuiz = "Marvel Superheroes"
+            }
+        }
+        
+        if selectedTitle == "Science" {
+            if let gameController = segue.destinationViewController as? GameView {
+                gameController.selectedQuiz = "Science"
+            }
+        }*/
+        
+        if let gameController = segue.destinationViewController as? GameView {
+            gameController.selectedQuiz = selectedTitle
+        }
+        
+    }
+    
+    
 }
-
